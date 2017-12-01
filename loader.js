d7e7d2436d510b82dc3875fb11ec98f897ed50e6
Cswl Coldwind <cswl1337@gmail.com>
2017-12-01 09:59:50 +0545

    Add please-upgrade-node

--- loader.js
+++ loader.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+// Must run BEFORE requiring other modules
+const pkg = require('./package.json')
+require('please-upgrade-node')(pkg)
+
 // entry point for @std/esm
 require = require("@std/esm")(module);
 module.exports = require("./src/main.mjs").default;
